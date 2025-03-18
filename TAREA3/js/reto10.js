5255d90b79148b6cc2052b024e285c77694eb091
laubenavides97 <laurabegam@gmail.com>
2025-03-18 14:25:10 -0600

    updates en el reto 10

--- TAREA3/js/reto10.js
+++ TAREA3/js/reto10.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const resultadoContainer = document.getElementById('resultado');
     const buscar = document.getElementById('btnAccion');
 
-    buscar.addEventListener('click', function() {
+    buscar.addEventListener('click', function () {
         //funcion trim que permite eliminar espacios en blanco al inicio y al final
         const cedula = cedulaInput.value.trim();
-        
+
         //verifica si el campo de cedula esta vacio
         if (cedula.length == 0) {
             Swal.fire({
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     mostrarInformacionEmpleado(cedula);
                 } else {
                     Swal.fire({
-                        icon: 'error',
+                        html: '<dotlottie-player src="https://lottie.host/f7f2093e-c0f6-4b32-8ee4-eca207002d8e/E88K7wCt3o.lottie" background="transparent" speed="0.5" autoplay></dotlottie-player>',
                         title: "La cédula ingresada no es válida",
                         confirmButtonText: "Intentar de nuevo",
                         scrollbarPadding: false,
@@ -67,11 +67,11 @@ document.addEventListener('DOMContentLoaded', function () {
     function mostrarInformacionEmpleado(cedula) {
         //objeto llamado empleados que guarda todos los datos del empleado
         const empleados = {
-            '109110338': { nombre: 'Andrés', apellidos: 'Gómez Ramírez', foto: '../imagenes/usuario1.jpg', departamento: 'Logística y Operaciones' },
-            '209110338': { nombre: 'Sofía', apellidos: ' Fernández Torres', foto: '../imagenes/usuario2.jpg', descripcion: 'Diseñadora UX-UI' },
-            '309110338': { nombre: 'Mateo', apellidos: ' Vargas León', foto: '../imagenes/usuario3.jpg', descripcion: 'Programador Web' },
-            '409110338': { nombre: 'Valentina', apellidos: ' Ruiz Castillo', foto: '../imagenes/usuario4.jpg', descripcion: 'Diseñadora gráfica' },
-            '509110338': { nombre: 'Sebastián', apellidos: ' López Herrera', foto: '../imagenes/usuario5.jpg', descripcion: 'Analista de sistema' },
+            '109110338': { nombre: 'Andrés', apellidos: 'Gómez Ramírez', foto: '../imagenes/usuario1.png', departamento: 'Logística y Operaciones' },
+            '209110338': { nombre: 'Sofía', apellidos: ' Fernández Torres', foto: '../imagenes/usuario2.png', departamento: 'Diseño UX/UI' },
+            '309110338': { nombre: 'Mateo', apellidos: ' Vargas León', foto: '../imagenes/usuario3.png', departamento: 'ProgramaciónWeb' },
+            '409110338': { nombre: 'Valentina', apellidos: ' Ruiz Castillo', foto: '../imagenes/usuario4.png', departamento: 'Diseño Gráfico' },
+            '509110338': { nombre: 'Sebastián', apellidos: ' López Herrera', foto: '../imagenes/usuario5.png', departamento: 'Analisis de sistemas' },
         };
         //condicional que permite validar si el usuario existe
         if (empleados[cedula]) {
@@ -81,9 +81,16 @@ document.addEventListener('DOMContentLoaded', function () {
             mostrarResultado(empleado);
         } else {
             Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'El usuario no exixte...'
+                html: '<dotlottie-player src="https://lottie.host/f7f2093e-c0f6-4b32-8ee4-eca207002d8e/E88K7wCt3o.lottie" background="transparent" speed="0.5" autoplay></dotlottie-player>',
+                title: "El usuario no existe",
+                confirmButtonText: "Intentar de nuevo",
+                scrollbarPadding: false,
+                customClass: {
+                    title: "titlePopUp",
+                    htmlContainer: "popUpContainer",
+                    popup: "popup",
+                    confirmButton: "btnPopUp"
+                }
             });
         }
     }
@@ -92,15 +99,14 @@ document.addEventListener('DOMContentLoaded', function () {
     function mostrarResultado(empleado) {
         //permite crear contenido HTML usando la propieda innerHTML
         resultadoContainer.innerHTML = `
-            <div class="card" style="width: 18rem;">
-                        <img src="imagenes/${empleado.foto}" class="card-img-top" alt="Foto del empleado">
-                        <div class="card-body">
-                            <h5 class="card-title">${empleado.nombre}</h5>
-                            <p class="card-text">Descripción: ${empleado.descripcion}</p>
-                            <p class="card-text">Jornada: ${empleado.jornada}</p>
-                            <p class="card-text">Salario: ${empleado.salario}</p>
-                        </div>
-                    </div>
+        <div id="resultado" class="resultado">
+            <img src="imagenes/${empleado.foto}" alt="Foto del empleado">
+            <div class="cardEmpleado">
+                <span class="hRetos2">Nombre: ${empleado.nombre}</span>
+                <span class="pResultado">Apellidos: ${empleado.apellidos}</span>
+                <span class="pResultado">Departamento: ${empleado.departamento}</span>
+            </div>
+        </div>
                 `;
     }
 
