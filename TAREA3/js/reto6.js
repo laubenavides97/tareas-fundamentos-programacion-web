5ab8f4e5e319879b9989bb721c59e69a7eb4aa9d
laubenavides97 <laurabegam@gmail.com>
2025-03-19 01:20:19 -0600

    reto 6

---
+++ TAREA3/js/reto6.js
@@ -0,0 +1,19 @@
+function color1() {
+    document.getElementById("vehiculo").src = "imagenes/color1.png";
+}
+
+function color2() {
+    document.getElementById("vehiculo").src = "imagenes/color2.png";
+}
+
+function color3() {
+    document.getElementById("vehiculo").src = "imagenes/color3.png";
+}
+
+function color4() {
+    document.getElementById("vehiculo").src = "imagenes/color4.png";
+}
+
+function color5() {
+    document.getElementById("vehiculo").src = "imagenes/color5.png";
+}
